6cd027615d159f6b618c4eff6d9162ee4f988981
kalkeesh jami <109733774+kalkeesh@users.noreply.github.com>
2024-10-22 17:41:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 class EmailSchema(BaseModel):
